1728484e3aa078667225f980f8b4ca77159d59f9
MaayanMashhadi <maayanmas4@gmail.com>
2023-10-01 09:01:03 +0300

    start login

--- Simulations/User/src/Main.java
+++ Simulations/User/src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import controllers.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("screens/LoginScene.fxml"));
+        Parent root = fxmlLoader.load();
+        LoginController controller = fxmlLoader.getController();
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Predictions");
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
+
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+
+
 }
